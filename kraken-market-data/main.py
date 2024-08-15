b90253ae87c2adda8bfad03ec61d9f10f7197c4d
chungungo11 <69803244+chungungo11@users.noreply.github.com>
2024-08-16 01:53:27 +0200

    add call_endpoint, get_asset_info and restart_prompt

--- kraken-market-data/main.py
+++ kraken-market-data/main.py
@@ -21,8 +21,8 @@ public_endpoints = {
     "get_recent_spreads": "/public/Spread"
 }
 
-def print_endpoints():
-    print(f"\n***********************\n* Available endpoints *\n***********************")
+def start_program():
+    print(f"\n-----------------------\n  Available Endpoints  \n-----------------------")
     for endpoint in public_endpoints:
         print(f"- {endpoint}")
     get_user_input()
@@ -31,20 +31,87 @@ def get_user_input():
     print("\nEnter the endpoint you would like to call (e.g. 'get_asset_info'):")
     user_input = input("> ")
     if validate_user_input(user_input):
-        endpoint = public_endpoints[user_input]
-        call_endpoint(endpoint)
+        request = user_input
+        call_endpoint(request)
 
 def validate_user_input(user_input):
     if user_input in public_endpoints:
         return True
     else:
-        print("Your input was invalid. Please try again.")
+        print("\nYour input was invalid. Please check for any typos and try again.")
         get_user_input()
 
-def call_endpoint(endpoint):
-    url = BASE_URL + endpoint
+def call_endpoint(request):
+    endpoint = public_endpoints[request]
+    if request == "get_server_time" or request == "get_system_status":
+        url = BASE_URL + endpoint
+        get_response(url)
+    elif request == "get_asset_info":
+        get_asset_info(endpoint)
+    elif request == "get_tradable_asset_pairs":
+        get_tradable_asset_pairs(endpoint)
+    elif request == "get_ticker_information":
+        get_ticker_information(endpoint)
+    elif request == "get_ohlc_data":
+        get_ohlc_data(endpoint)
+    elif request == "get_order_book":
+        get_order_book(endpoint)
+    elif request == "get_recent_trades":
+        get_recent_trades(endpoint)
+    elif request == "get_recent_spreads":
+        get_recent_spreads(endpoint)
+
+def get_asset_info(endpoint):
+    print("\nOPTIONAL: Comma delimited list of assets to get info on. Default: all available assets.")
+    asset = input("> ")
+    
+    # print("OPTIONAL: Asset class. Default: 'currency'.")
+
+    if asset == "":
+        url = BASE_URL + endpoint
+    else:
+        url = f"{BASE_URL}{endpoint}?asset={asset}"
+        
+    get_response(url)
+
+def get_tradable_asset_pairs(endpoint):
+    pass
+
+def get_ticker_information(endpoint):
+    pass
+
+def get_ohlc_data(endpoint):
+    pass
+
+def get_order_book(endpoint):
+    pass
+
+def get_recent_trades(endpoint):
+    pass
+
+def get_recent_spreads(endpoint):
+    pass
+
+def get_response(url):
+    print("\nRequest url:" , url)
     response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.text)
+    print_response(response)
+    restart_prompt()
+
+def print_response(response):
+    pretty_response = json.dumps(response.json(), indent=2)
+    print(f"\n------------\n  Response  \n------------\n{pretty_response}")
+    
+def restart_prompt():
+    print("\nWould you like to call another endpoint? (y/n)")
+    restart = input("> ")
+    if restart.lower() == "y":
+        start_program()
+    elif restart.lower == "n":
+        exit(0)
+    # else:
+    #     print("\nYour input was invalid. Please type 'y' to restart or 'n' to exit.")
+    #     return restart_prompt()
 
 if __name__ == "__main__":
-    print_endpoints()
+    start_program()
