28b50edb3c6d0afc86106455183d34c08b8fbe0c
chungungo11 <69803244+chungungo11@users.noreply.github.com>
2024-08-18 16:04:46 +0200

    add get_ohlc_data

--- kraken-market-data/main.py
+++ kraken-market-data/main.py
@@ -126,7 +126,40 @@ def get_ticker_information(endpoint):
     get_response(url)
 
 def get_ohlc_data(endpoint):
-    pass
+    print("\nREQUIRED: Asset pair to get data for.")
+    print("\nExample: 'XBTUSD'")
+    pair = input("> ")
+    if pair == "":
+        print("\nAsset pair is required! Please enter an asset pair.")
+        get_ohlc_data(endpoint)
+
+    interval_values = [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]
+    print("\nOPTIONAL: Time frame interval in minutes. Default value: '1'.")
+    print("\nPossible values: [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]")
+    interval = input("> ")
+    if int(interval) not in interval_values:
+        print("\nInterval value invalid! Please enter a valid interval.")
+        get_ohlc_data(endpoint)
+
+    print("\nOPTIONAL: Return up to 720 OHLC data points since given timestamp.")
+    print("\nExample: '1548111600'")
+    since = input("> ")
+
+    # pair
+    if pair != "" and interval == "" and since == "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}"
+    # pair + interval
+    elif pair != "" and interval != "" and since == "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&interval={interval}"
+    # pair + since
+    elif pair != "" and interval == "" and since != "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&since={since}"
+    # pair + interval + since
+    elif pair != "" and interval != "" and since != "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&interval={interval}&since={since}"
+    
+    # validate_response(url)
+    get_response(url)
 
 def get_order_book(endpoint):
     pass
