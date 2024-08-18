3ae0bf0ce435eb1bb09405a5a675b8d31dd2c48f
chungungo11 <69803244+chungungo11@users.noreply.github.com>
2024-08-18 16:31:37 +0200

    add get_order_book, get_recent_trades, get_recent_spreads

--- kraken-market-data/main.py
+++ kraken-market-data/main.py
@@ -43,9 +43,10 @@ def validate_user_input(user_input):
 
 def call_endpoint(request):
     endpoint = public_endpoints[request]
-    if request == "get_server_time" or request == "get_system_status":
-        url = BASE_URL + endpoint
-        get_response(url)
+    if request == "get_server_time":
+        get_server_time(endpoint)
+    elif request == "get_system_status":
+        get_server_status(endpoint)
     elif request == "get_asset_info":
         get_asset_info(endpoint)
     elif request == "get_tradable_asset_pairs":
@@ -61,9 +62,20 @@ def call_endpoint(request):
     elif request == "get_recent_spreads":
         get_recent_spreads(endpoint)
 
+def get_server_time(endpoint):
+    print("\nINFO: Get the server's time.")
+    url = BASE_URL + endpoint
+    get_response(url)
+
+def get_server_status(endpoint):
+    print("\nINFO: Get the current system status or trading mode.")
+    url = BASE_URL + endpoint
+    get_response(url)
+
 def get_asset_info(endpoint):
+    print("\nINFO: Get information about the assets that are available for deposit, withdrawal, trading and earn.")
     print("\nOPTIONAL: Comma delimited list of assets to get info on. Default: all available assets.")
-    print("\nExample: 'BTC,ETH'")
+    print("EXAMPLE: 'BTC,ETH'")
     asset = input("> ")
     
     # print("OPTIONAL: Asset class. Default: 'currency'.")
@@ -77,15 +89,16 @@ def get_asset_info(endpoint):
     get_response(url)
 
 def get_tradable_asset_pairs(endpoint):
+    print("\nINFO: Get tradable asset pairs.")
     print("\nOPTIONAL: Asset pair(s) to get data for. Default: all available assets.")
-    print("\nExample: 'BTC/USD,ETH/BTC'")
+    print("EXAMPLE: 'BTC/USD,ETH/BTC'")
     pair = input("> ")
 
     print("\nOPTIONAL: Info to retrieve: 'info', 'leverage', 'fees', 'margin'. Default: 'info'.")
     info = input("> ")
 
     print("\nOPTIONAL: Filter for response to only include pairs available in provided countries/regions. Default: all countries/regions.")
-    print("\nExample: 'US:TX,GB,CA'")
+    print("EXAMPLE: 'US:TX,GB,CA'")
     country_code = input("> ")
 
     # all query params are empty
@@ -113,8 +126,11 @@ def get_tradable_asset_pairs(endpoint):
     get_response(url)
 
 def get_ticker_information(endpoint):
+    print("\nGet ticker information for all or requested markets. To clarify usage, note that:")
+    print("- Today's prices start at midnight UTC")
+    print("- Leaving the pair parameter blank will return tickers for all tradeable assets on Kraken")
     print("\nOPTIONAL: Asset pair to get data for. Default: all tradeable exchange pairs.")
-    print("\nExample: 'XBTUSD'")
+    print("EXAMPLE: 'XBTUSD'")
     pair = input("> ")
 
     if pair == "":
@@ -126,8 +142,9 @@ def get_ticker_information(endpoint):
     get_response(url)
 
 def get_ohlc_data(endpoint):
+    print("\nINFO: GET OHLC information. The last entry in the OHLC array is for the current, not-yet-committed frame and will always be present, regardless of the value of since.")
     print("\nREQUIRED: Asset pair to get data for.")
-    print("\nExample: 'XBTUSD'")
+    print("EXAMPLE: 'XBTUSD'")
     pair = input("> ")
     if pair == "":
         print("\nAsset pair is required! Please enter an asset pair.")
@@ -135,14 +152,14 @@ def get_ohlc_data(endpoint):
 
     interval_values = [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]
     print("\nOPTIONAL: Time frame interval in minutes. Default value: '1'.")
-    print("\nPossible values: [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]")
+    print("POSSIBLE VALUES: [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]")
     interval = input("> ")
     if int(interval) not in interval_values:
         print("\nInterval value invalid! Please enter a valid interval.")
         get_ohlc_data(endpoint)
 
     print("\nOPTIONAL: Return up to 720 OHLC data points since given timestamp.")
-    print("\nExample: '1548111600'")
+    print("EXAMPLE: '1548111600'")
     since = input("> ")
 
     # pair
@@ -162,13 +179,89 @@ def get_ohlc_data(endpoint):
     get_response(url)
 
 def get_order_book(endpoint):
-    pass
+    print("\nINFO: Returns the Order Book.")
+    print("\nREQUIRED: Asset pair to get data for.")
+    print("EXAMPLE: 'XBTUSD'")
+    pair = input("> ")
+    if pair == "":
+        print("\nAsset pair is required! Please enter an asset pair.")
+        get_order_book(endpoint)
+
+    print("\nOPTIONAL: Maximum number of asks/bids. Default value: '100'.")
+    print("POSSIBLE VALUES: >= 1 and <= 500")
+    count = input("> ")
+    if int(count) not in range(1, 501):
+        print("\Count value invalid! Please enter a valid interval.")
+        get_order_book(endpoint)
+
+    # pair
+    if pair != "" and count == "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}"
+    # pair + count
+    elif pair != "" and count != "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&count={count}"
+
+    # validate_response(url)
+    get_response(url)
 
 def get_recent_trades(endpoint):
-    pass
+    print("\nINFO: Returns the last 1000 trades by default.")
+    print("\nREQUIRED: Asset pair to get data for.")
+    print("EXAMPLE: 'XBTUSD'")
+    pair = input("> ")
+    if pair == "":
+        print("\nAsset pair is required! Please enter an asset pair.")
+        get_recent_trades(endpoint)
+
+    print("\nOPTIONAL: Return trade data since given timestamp.")
+    print("EXAMPLE: '1616663618'")
+    since = input("> ")
+    
+    print("\nOPTIONAL: Return specific number of trades, up to 1000. Default value: '1000'.")
+    print("POSSIBLE VALUES: >= 1 and <= 1000")
+    count = input("> ")
+    if int(count) not in range(1, 1001):
+        print("\Count value invalid! Please enter a valid number of trades.")
+        get_recent_trades(endpoint)
+
+    # pair
+    if pair != "" and since == "" and count == "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}"
+    # pair + since
+    elif pair != "" and since != "" and count == "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&since={since}"
+    # pair + count
+    elif pair != "" and since == "" and count != "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&count={count}"
+    # pair + since + count
+    elif pair != "" and since != "" and count != "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&since={since}&count={count}"
+
+    # validate_response(url)
+    get_response(url)
 
 def get_recent_spreads(endpoint):
-    pass
+    print("\nINFO: Returns the last ~200 top-of-book spreads for a given pair.")
+    print("\nREQUIRED: Asset pair to get data for.")
+    print("EXAMPLE: 'XBTUSD'")
+    pair = input("> ")
+    if pair == "":
+        print("\nAsset pair is required! Please enter an asset pair.")
+        get_recent_spreads(endpoint)
+
+    print("\nOPTIONAL: Returns spread data since given timestamp. Intended for incremental updates within available dataset (does not contain all historical spreads).")
+    print("EXAMPLE: '1678219570'")
+    since = input("> ")
+
+    # pair
+    if pair != "" and since == "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}"
+    # pair + since
+    elif pair != "" and since != "":
+        url = f"{BASE_URL}{endpoint}?pair={pair}&since={since}"
+
+    # validate_response(url)
+    get_response(url)
 
 def validate_response(url):
     pass
