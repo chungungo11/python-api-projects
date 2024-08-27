c0b5dc584e3ec0d633fe8fd79b66b34506195079
chungungo11 <69803244+chungungo11@users.noreply.github.com>
2024-08-27 18:38:27 +0200

    fix interval input validation

--- kraken-market-data/api/get_ohlc_data.py
+++ kraken-market-data/api/get_ohlc_data.py
@@ -22,11 +22,11 @@ def get_ohlc_data(endpoint):
     )
     print("\nPOSSIBLE VALUES: [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]")
     interval = input("> ")
-    if int(interval) not in interval_values:
+    while interval != "" and int(interval) not in interval_values:
         print(
-            f"\n{Fore.RED}Interval value invalid. Please enter a valid interval.{Style.RESET_ALL}"
+            f"\n{Fore.RED}Value is invalid. Please enter a valid time interval.{Style.RESET_ALL}"
         )
-        get_ohlc_data(endpoint)
+        interval = input("> ")
 
     print(
         f"\n{Fore.YELLOW}OPTIONAL: Return up to 720 OHLC data points since given timestamp.{Style.RESET_ALL}"
