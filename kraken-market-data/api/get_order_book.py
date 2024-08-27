8a20650894888b5328655a5413c6369b55797b29
chungungo11 <69803244+chungungo11@users.noreply.github.com>
2024-08-27 18:43:13 +0200

    fix input validation

--- kraken-market-data/api/get_order_book.py
+++ kraken-market-data/api/get_order_book.py
@@ -10,22 +10,22 @@ def get_order_book(endpoint):
     print(f"\n{Fore.YELLOW}REQUIRED: Asset pair to get data for.{Style.RESET_ALL}")
     print("\nEXAMPLE: 'XBTUSD'")
     pair = input("> ")
-    if pair == "":
+    while pair == "":
         print(
             f"\n{Fore.RED}Asset pair is required. Please enter an asset pair.{Style.RESET_ALL}"
         )
-        get_order_book(endpoint)
+        pair = input("> ")
 
     print(
         f"\n{Fore.YELLOW}OPTIONAL: Maximum number of asks/bids. Default value: '100'.{Style.RESET_ALL}"
     )
     print("\nPOSSIBLE VALUES: >= 1 and <= 500")
     count = input("> ")
-    if int(count) not in range(1, 501):
+    while count != "" and int(count) not in range(1, 501):
         print(
-            f"\n{Fore.RED}Value is invalid. Please enter a valid interval.{Style.RESET_ALL}"
+            f"\n{Fore.RED}Value is invalid. Please enter a valid number of asks/bids.{Style.RESET_ALL}"
         )
-        get_order_book(endpoint)
+        count = input("> ")
 
     # pair
     if pair != "" and count == "":
